ee4547a0dc04d69f424360a67a1f6d44c2920876
Isabelle <ohaga.isabelle@gmail.com>
2023-02-17 12:37:28 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,31 @@
 
 /**
  * main - Prints all possible different combinations of two digits
+ *
  * Return: 0
  */
 int main(void)
 {
 	int i, j;
-
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			if (i != j)
+			if (i == 8 && j == 9)
 			{
-				putchar(i % 10 + '0');
-				putchar(j % 10 + '0');
-				if (i < 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(i + '0');
+				putchar(j + '0');
+			}
+			else
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	purchar('\n');
+	putchar('\n');
 
 	return (0);
 }
