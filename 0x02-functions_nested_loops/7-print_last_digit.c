ee498cf4fd0e25afa89325c6a1d7670f52e1f362
Isabelle <ohaga.isabelle@gmail.com>
2023-02-21 07:37:09 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,19 @@
 /**
  * print_last_digit - prints the last digit
  * @n: the passed arguments
- * Return: the last digit
+ * Return: integer value
  */
+
 int print_last_digit(int n)
 {
-	int x;
+	int last;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0')
-	return (x);
+	last = n % 10
+
+	if (last < 0)
+		last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
