5cd9537b23359a6f47adcdf3b631834f2d84bcc9
Isabelle <ohaga.isabelle@gmail.com>
2023-02-21 11:46:45 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,24 +11,19 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (num = 0; num <= 9; num++)
+		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
 
-			for (mult = 1; mult <= 9; mult++)
-			{
-				_putchar(',');
+			prod = num * mult;
+
+			if (prod <= 9)
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
-					_putchar(' ');
-				else
-					_putchar((prod / 10) + '0');
-				_putchar9((prod % 10) + '0');
-			}
-			_putchar('\n');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n')
 	}
 }
