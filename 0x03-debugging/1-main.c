19f3e497174791980bcdf739c26bee7faab773a7
Isabelle <ohaga.isabelle@gmail.com>
2023-02-22 09:15:41 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,9 +15,9 @@ printf("Infinte loop incoming :(\n");
 i = 0;
 
 	/**
-	 *  while (i < 10)
+	 * while (i < 10)
 	 * {
-	 *	putchar(i);
+	 * putchar(i);
 	 * }
 	 */
 printf("Infinite loop avoided! \\o/\n");
