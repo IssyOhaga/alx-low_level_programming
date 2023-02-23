3a4af1f2a41a94088fb6cabd022d11ecec0c45c9
Isabelle <ohaga.isabelle@gmail.com>
2023-02-23 13:01:12 -0800

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining days(int, int, int);
+void print_remaining_days(int, int, int);
 int convert_day(int, int);
 void print_times_table(int n);
 
