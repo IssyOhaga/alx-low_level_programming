cf5edf0489c0367a707066252869a0b38872e86e
Isabelle <ohaga.isabelle@gmail.com>
2023-02-27 11:04:43 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: the string pointer
+ * Return: index
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{}
+	return (index);
+}
