74714bb2341f6376411d63580009bd1862f821a4
Isabelle <ohaga.isabelle@gmail.com>
2023-03-06 06:17:27 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_diagsums - function definition
+ * @a: the array to be used
+ * @size: the size of the diagonal
+ * Return: void
+ */
+
 void print_diagsums(int *a, int size)
 {
 	int index;
