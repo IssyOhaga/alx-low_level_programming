5d58eef058e11095e1a7dc12087f8d66afc67887
Isabelle Ohaga <ohaga.isabelle@gmail.com>
2023-04-05 16:40:20 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
-	while (*head != NULL)
+
+	while (*head)
 	{
-		tmp = *head;
+		temp = (*hea)->next;
 		free(*head);
-		*head = tmp->next;
+		*head = temp;
 	}
 	*head = NULL;
 }
