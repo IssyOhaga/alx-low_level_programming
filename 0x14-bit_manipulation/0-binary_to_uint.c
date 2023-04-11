349128cfee16565296cbf6994baa21c8bd548d3d
Isabelle Ohaga <ohaga.isabelle@gmail.com>
2023-04-11 13:37:52 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * binary_to_unit - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing binary number
  *
  * Return: converted number
