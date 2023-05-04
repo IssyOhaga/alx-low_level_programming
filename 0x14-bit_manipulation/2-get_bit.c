99a7a6de1d2521e9d12222a4a145167e6872b35e
Issy Ohaga <ohaga.isabelle@gmail.com>
2023-05-04 15:02:24 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,12 +11,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-int bit;
+	int bit;
 
-if (index >= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 	return (-1);
 
-bit = (n >> index) & 1;
+	bit = (n >> index) & 1;
 
-return (bit);
+	return (bit);
 }
