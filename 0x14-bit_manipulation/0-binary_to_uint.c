99f9c0e3da122d4686495d5cc7b5beb927d1027b
Issy Ohaga <ohaga.isabelle@gmail.com>
2023-05-04 11:05:32 -0700

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,11 +16,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	for (len = 0; b[len]; len++)
+	while (*b)
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-	}
+	if (!isdigit(*b) || (*b != '0' && *b != '1'))
+	return (0);
 
 	num <<= 1;
 	num += (*b++ - '0');
