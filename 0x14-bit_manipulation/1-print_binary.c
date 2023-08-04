df5961790ecabb70a0a738e391130e7535e21778
Isabelle <ohaga.isabelle@gmail.com>
2023-08-03 17:12:53 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+int i, flag = 0;
+
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+
+for (i = 63; i >= 0; i--)
+{
+if ((n >> i) & 1)
+{
+flag = 1;
+_putchar ('1');
+}
+else if (flag)
+_putchar('0');
+}
+}
